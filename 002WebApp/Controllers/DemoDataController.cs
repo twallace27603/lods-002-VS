dd076addc1a2994bec0b74ed3d6eb3e6f2708f68
twallace27603 <tswwci@hotmail.com>
2018-04-15 07:53:14 -0400

    Initial load

---
+++ 002WebApp/Controllers/DemoDataController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using _002WebApp.Models;
+
+namespace _002WebApp.Controllers
+{
+    public class DemoDataController : ApiController
+    {
+        // GET: api/DemoData
+        public IEnumerable<CLFinal> Get()
+        {
+            return (new ChampionsLeagueContext()).GetResults();
+        }
+
+        // GET: api/DemoData/5
+        public CLFinal Get(int id)
+        {
+            return (new ChampionsLeagueContext()).GetResults().First(cl => cl.SeasonId == id);
+        }
+
+ 
+    }
+}
---
+++ 002WebApp/Models/ChampionsLeagueContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+namespace _002WebApp.Models
+{
+    public class ChampionsLeagueContext
+    {
+        private string connString = System.Configuration.ConfigurationManager.ConnectionStrings["demoData"].ConnectionString;
+
+        public List<CLFinal> GetResults()
+        {
+            var conn = new SqlConnection(connString);
+            var SQL = "Select * From dbo.ChampionsLeague";
+            var cmd = new SqlCommand(SQL, conn);
+            var results = new List<CLFinal>();
+            conn.Open();
+            try
+            {
+                var rdr = cmd.ExecuteReader();
+                while (rdr.Read()) {
+                    results.Add(new CLFinal { SeasonId = (int)rdr["SeasonId"], Season = rdr["Season"].ToString(), Score = rdr["Score"].ToString(), SecondPlace = rdr["SecondPlace"].ToString(), Winner = rdr["Winner"].ToString() });
+                }
+
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return results;
+        }
+    }
+    public class CLFinal
+    {
+        public int SeasonId { get; set; }
+        public string Season { get; set; }
+        public string Winner { get; set; }
+        public string SecondPlace { get; set; }
+        public string Score { get; set; }
+    }
+}
